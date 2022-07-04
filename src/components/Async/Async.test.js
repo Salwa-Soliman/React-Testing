6c3a6a7c81ddde1877aa7c2bb3b6e2b355a062ba
Salwa A. Soliman <86159119+Salwa-Soliman@users.noreply.github.com>
2022-07-04 14:58:37 +0200

    working with mocks

--- src/components/Async/Async.test.js
+++ src/components/Async/Async.test.js
@@ -13,4 +13,23 @@ describe("Async Component", () => {
     const listItems = await screen.findAllByRole("listitem");
     expect(listItems).not.toHaveLength(0);
   });
+
+  test("mocking fetch method", async () => {
+    //Arrange
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
+
+    render(<Async />);
+
+    //Act
+    // ... nothing
+
+    //Assert
+    const listItems = await screen.findAllByRole("listitem");
+
+    expect(listItems).toHaveLength(1);
+    expect(listItems).not.toHaveLength(0);
+  });
 });
